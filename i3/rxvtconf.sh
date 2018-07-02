3e8974e36065b712a0c6dbc6e40a99c94177248a
Solace Greyowl <elijahjuanye@gmail.com>
2018-07-02 07:39:09 -0700

    add i3,polybar config files

---
+++ i3/rxvtconf.sh
@@ -0,0 +1,4 @@
+echo "URxvt.font: xft:Hack:size=10" | xrdb -merge
+echo "URxvt.scrollBar: false" | xrdb -merge
+echo "urxvt*background: rgba:0000/0000/0200/c800" | xrdb -merge
+echo "URxvt.perl-ext-common: ...,tabbed,..." | xrdb -merge
