d2c2f0e7567959a7381aba4db1bb8f5ad50e0069
Solace Greyowl <elijahjuanye@gmail.com>
2018-07-11 01:06:07 -0700

    Alias file, refind and boot config

--- audio_setup.sh
+++ audio_setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 exec cadence-session-start --start
-exec --no-startup-id aj-snapshot -d /home/solace/Documents/Projects/Audio/ajsnapshots/default
+exec aj-snapshot -d /home/solace/Documents/Projects/Audio/ajsnapshots/default
+cadence
