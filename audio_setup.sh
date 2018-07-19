d5f63cc70878a3916f3655948640cc68e6816c7e
Solace Greyowl <elijahjuanye@gmail.com>
2018-07-19 07:01:49 -0700

    fixing mistakes

--- audio_setup.sh
+++ audio_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -f "/var/log/audiosetuplog" ]; then
+    echo `date` >> /var/log/audiosetuplog
+    echo ""
+fi
+
 echo "Starting JACK" >> /var/log/audiosetuplog
 jack_control start
 jack_control ds alsa
@@ -8,16 +13,22 @@ jack_control dps nperiods 3
 #Buffer
 jack_control dps period 1024
 sleep 10
-echo "Connecting a2j MIDI"
+echo "Connecting a2j MIDI" >> /var/log/audiosetuplog
 a2jmidid -ue &
 sleep 10
 cadence --minimized &
 pactl set-default-sink jack_out
 notify-send "JACK Audio Connection Kit is connected and set for MIDI"
-
-echo "Peripheral Check"
+echo "Jack Setup Complete" >> /var/log/audiosetuplog
+echo "Peripheral Check" >> /var/log/audiosetuplog
 if [ -d "/proc/asound/Mic" ]; then
-		echo "Samson Meteor Mic is Connected"
+		echo "Samson Meteor Mic is Connected" >> /var/log/audiosetuplog
 		samson_mic
 		samson_speaker
+fi
+
+echo "Done" >> /var/log/audiosetuplog
+
+if [ wc --lines /var/log/audiosetuplog > 32]; then
+    rm /var/log/audiosetuplog
 fi
--- display_setup.sh
+++ display_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ `xrandr --query | grep '\bconnected\b'` | `wc --lines` > 1 ]; then 
-    `xrandr --output eDP-1-1 --auto --left-of DP-0`
+if [ `xrandr --query | grep '\bconnected\b' | wc --lines` > 1 ]; then 
+    `xrandr --output DP-0 --auto --left-of eDP-1-1`
 else
-    `xrandr --output DP-0 --primary --auto`
+    `xrandr --output eDP-1-1 --primary --auto`
 fi
