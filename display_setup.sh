0165cb656699097901b35e0373ba75bada54cc13
Solace Greyowl <elijahjuanye@gmail.com>
2018-07-21 22:51:22 -0700

    Multi monitors...smh

--- display_setup.sh
+++ display_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ `xrandr --query | grep '\bconnected\b' | wc --lines` > 1 ]; then 
     `xrandr --output DP-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
-    respolybar
+    res-polybar
 else
     `xrandr --output eDP-1-1 --primary --auto --output DP-0 --off`
 fi
