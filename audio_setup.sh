12c738532b0134ab929425d427580a59dab17481
Solace Greyowl <elijahjuanye@gmail.com>
2018-08-11 22:17:08 -0700

    I've solved my problems...hopefully

--- audio_setup.sh
+++ audio_setup.sh
@@ -14,7 +14,7 @@ jack_control dps nperiods 3
 jack_control dps period 1024
 sleep 10
 echo "Connecting a2j MIDI" >> /var/log/audiosetuplog
-a2jmidid -ue
+a2jmidid -ue &
 sleep 10
 pactl set-default-sink jack_out
 pactl set-default-source jack_in
@@ -28,10 +28,14 @@ if [ -d "/proc/asound/Mic" ]; then
 		samson_speaker
 fi
 
+echo "Starting Cadence" >> /var/log/audiosetuplog
 cadence --minimized &
 
 echo "Done" >> /var/log/audiosetuplog
 
 if [ wc --lines /var/log/audiosetuplog > 32]; then
     rm /var/log/audiosetuplog
+    touch /var/log/audiosetuplog
 fi
+
+exit 1
