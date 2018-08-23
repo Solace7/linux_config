6ce73fbd29c84c4273a2aa367656c203eeac2e46
Solace Greyowl <elijahjuanye@gmail.com>
2018-08-23 12:37:35 -0700

    misc updates

--- display_setup.sh
+++ display_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 if [ `xrandr --query | grep '\bconnected\b' | wc --lines` > 1 ]; then 
-    `xrandr --output DP-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
+#    `xrandr --output DP-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
+    `xrandr --output HDMI-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
     res-polybar
 else
-    `xrandr --output eDP-1-1 --primary --auto --output DP-0 --off`
+    `xrandr --output eDP-1-1 --primary --auto --output HDMI-0 --off`
 fi
