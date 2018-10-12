abc4a49ae0893e1fc2b86839698349f196f39def
Solace_Greyowl <solaceiiix@gmail.com>
2018-10-11 17:00:50 -0700

    Made logfile a variable and fixed a typo

--- scripts/audio_setup.sh
+++ scripts/audio_setup.sh
@@ -1,62 +1,66 @@
 #!/bin/bash
+
+LOG=$HOME/.local/var/log/audiosetuplog
+
 if [ -f "/home/sgreyowl/.local/var/log/audiosetuplog" ]; then
-  echo -e `date` "\n" >> $HOME/.local/var/log/audiosetuplog
+  echo -e `date` "\n" >> $LOG
 else
-  touch $HOME/.local/var/log/audiosetuplog
-  echo -e `date` "\n" >> $HOME/.local/var/log/audiosetuplog
+  touch $LOG
+  echo -e `date` "\n" >> $LOG
 fi
 
 #alsactl store
-echo "Starting JACK" >> $HOME/.local/var/log/audiosetuplog
+echo "Starting JACK" >> $LOG
 
 if [ -d "/proc/asound/CODEC" ]; then
     #Audio Interface
-    echo "Audio Interface Detected" >> $HOME/.local/var/log/audiosetuplog
-    jack_control dps device hw:CODEC | tee -a $HOME/.local/var/log/audiosetuplog
+    echo "Audio Interface Detected" >> $LOG
+    jack_control dps device hw:CODEC | tee -a $LOG
     #Separate outputs to connect via JACK
-    um2_ouputs &
+    um2_outputs &
 else
     #Internal Audio
-    jack_control dps device hw:PCH | tee -a $HOME/.local/var/log/audiosetuplog
+    echo "Using Internal Audio Card" >> $LOG
+    jack_control dps device hw:PCH | tee -a $LOG
 fi
 
-jack_control start | tee -a $HOME/.local/var/log/audiosetuplog
-jack_control ds alsa | tee -a $HOME/.local/var/log/audiosetuplog
+jack_control start | tee -a $LOG
+jack_control ds alsa | tee -a $LOG
 
-jack_control dps rate 44100 | tee -a $HOME/.local/var/log/audiosetuplog
-jack_control dps nperiods 3 | tee -a $HOME/.local/var/log/audiosetuplog
-jack_control dps period 512 | tee -a $HOME/.local/var/log/audiosetuplog
+jack_control dps rate 44100 | tee -a $LOG
+jack_control dps nperiods 3 | tee -a $LOG
+jack_control dps period 512 | tee -a $LOG
 sleep 10
-echo -e "Connecting a2j MIDI \n" >> $HOME/.local/var/log/audiosetuplog
+echo -e "Connecting a2j MIDI \n" >> $LOG
 killall -9 a2jmidid
-a2jmidid -ue & >> $HOME/.local/var/log/audiosetuplog
+a2jmidid -ue & >> $LOG
 sleep 10
 pactl set-default-sink jack_out
-echo "jack_out set as default sink" >> $HOME/.local/var/log/audiosetuplog
+echo "jack_out set as default sink" >> $LOG
 pactl set-default-source jack_in
-echo "jack_in set as default source" >> $HOME/.local/var/log/audiosetuplog
+echo "jack_in set as default source" >> $LOG
 #alsactl restore
 notify-send "JACK Audio Connection Kit is connected and set for MIDI"
-echo "Jack Setup Complete" >> $HOME/.local/var/log/audiosetuplog
-echo "Peripheral Check" >> $HOME/.local/var/log/audiosetuplog
+echo "Jack Setup Complete" >> $LOG
+echo "Peripheral Check" >> $LOG
 if [ -d "/proc/asound/Mic" ]; then
-		echo "Samson Meteor Mic is Connected" >> $HOME/.local/var/log/audiosetuplog
+		echo "Samson Meteor Mic is Connected" >> $LOG
 		samson_mic &
 #		samson_speaker
 fi
 
-echo -e "Starting Cadence \n" >> $HOME/.local/var/log/audiosetuplog
+echo -e "Starting Cadence \n" >> $LOG
 #cadence --minimized &
 #qjackctl &
 
-echo -e "Done \n" >> $HOME/.local/var/log/audiosetuplog
-echo `date` >> $HOME/.local/var/log/audiosetuplog
+echo -e "Done \n" >> $LOG
+echo `date` >> $LOG
 
-if [[ $(wc --lines $HOME/.local/var/log/audiosetuplog) > 32 ]]; then
-    rm $HOME/.local/var/log/audiosetuplog
-    touch $HOME/.local/var/log/audiosetuplog
-    echo "Audio Setup Completed. New log file created" >> $HOME/.local/var/log/audiosetuplog
-    echo `date` >> $HOME/.local/var/log/audiosetuplog
+if [[ $(wc --lines $LOG) > 32 ]]; then
+    rm $LOG
+    touch $LOG
+    echo "Audio Setup Completed. New log file created" >> $LOG
+    echo `date` >> $LOG
 fi
 
 exit 1
