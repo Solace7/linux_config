dc5f3f70ad3932c7f1e2cda7fd3228ee818c1e38
Solace_Greyowl <solaceiiix@gmail.com>
2018-11-11 06:34:43 -0800

    Moving Monitor

--- scripts/display_setup.sh
+++ scripts/display_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ `xrandr --query | grep '\bconnected\b' | wc --lines` > 1 ]; then 
 #    `xrandr --output HDMI-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
-    `xrandr --output HDMI-0 --auto --left-of eDP-1-1 --output eDP-1-1 --primary --auto`
+    `xrandr --output HDMI-0 --auto --right-of eDP-1-1 --output eDP-1-1 --primary --auto`
     #res-polybar
 else
     `xrandr --output eDP-1-1 --primary --auto --output HDMI-0 --off`
