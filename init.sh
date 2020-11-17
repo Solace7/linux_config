ce2b0555444d6c6a81bc515798987f1b04bb6ad7
Greyowl <solaceiiix@gmail.com>
2020-11-17 12:57:11 -0800

    From synergy to barrier

--- init.sh
+++ init.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Install Packages
-sudo pacman -Syyu tmux git zsh awesome neovim ranger rxvt-unicode feh w3m fzf fasd rofi wget 
+sudo pacman -Syu tmux git zsh awesome neovim ranger rxvt-unicode feh w3m fzf fasd rofi wget 
+
+if [[ ! $(which zsh) ]]; then
+	printf "ZSH Not installed\n"
+	sudo pacman -Syu zsh
+fi
 
 #Install ZSH
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Symlink files
+printf "Symlinking files...\n"
 (cd $HOME; ln -s $HOME/.config/.zshrc)
 (cd $HOME; ln -s $HOME/.config/.TerminalTweaksARCH)
 (cd $HOME; ln -s $HOME/.config/.profile)
 (cd $HOME; ln -s $HOME/.config/.tmuxline-snapshot)
 (cd $HOME; ln -s $HOME/.config/scripts/bin)
-
+printf "...done\n"
 # Misc
 
